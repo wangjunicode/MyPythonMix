a29b751b8fa8614d66e57e1cdcb20943613c978d
kyriewang <kyriewang@tencent.com>
2022-09-29 17:06:31 +0800

    通过F12  network 页签查看 网页请求的网址，
    -- python 直接请求一个网页链接

---
+++ GetHtmlText/GetRequest.py
@@ -0,0 +1,31 @@
+# https://lab.uwa4d.com/api/solution/projects/ids?categories[]=Unity&limit=24&skip=120&sort=update_time
+import json
+import requests
+
+
+cur_num = 0
+per_num = 100
+
+final_res = []
+
+for i in range(1, 10):
+    rsp1 = requests.get(
+        'https://lab.uwa4d.com/api/solution/projects/ids?categories[]=Unity&limit={num1}&skip{num2}&sort=update_time'.format(num1=per_num, num2=cur_num))
+
+    final_url = 'https://lab.uwa4d.com/api/solution/projects?'
+    rsp1Data = json.loads(rsp1.text)
+    for item in rsp1Data["ids"]:
+        final_url = final_url + 'ids[]=' + item+'&'
+
+    rsp2 = requests.get(final_url)
+    rsp2Data = json.loads(rsp2.text)
+
+    for item in rsp2Data["projects"]:
+        final_res.append(item["descCN"] + "  " + item["project_link"]+'\n')
+
+    cur_num = cur_num + per_num
+
+
+with open("GetHtmlText/1111.txt", "w", encoding="utf-8") as f:
+    for item in final_res:
+        f.write(item)
